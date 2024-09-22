2ac1ebf4ac552bc823f59a76d71f9d9e1a6f6c93
Jason Horgan <jasonahorgan@gmail.com>
2024-09-22 21:06:27 +0000

    add an appointment model

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class appointment(models.Model):
+    service = models.CharField(max_length=200, unique=True)
+    barber = models.SlugField(max_length=200, unique=True)
+    date = models.DateField()
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+)
+    created_on = models.DateTimeField(auto_now_add=True)
+    
+
+
+
