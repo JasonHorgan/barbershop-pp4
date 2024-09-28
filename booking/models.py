e60b5035bc88cf547dd17992080cf85a2959d75d
Jason Horgan <jasonahorgan@gmail.com>
2024-09-28 16:27:41 +0000

    Add time field to appointment model, discription to barber model and price to service model

--- booking/models.py
+++ booking/models.py
@@ -1,11 +1,14 @@
 from django.db import models
+from django.utils import timezone
 from django.contrib.auth.models import User
 
 # Create your models here.
+
 class Appointment(models.Model):
     service = models.CharField(max_length=200, unique=True)
     barber = models.SlugField(max_length=200, unique=True)
     date = models.DateField()
+    time = models.TimeField(null=True)
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="blog_posts"
 )
@@ -13,12 +16,15 @@ class Appointment(models.Model):
 
 class Barber(models.Model):
     name = models.CharField(max_length=100)
+    description = models.CharField(max_length=200, default='SOME STRING')
+    
 
     def __str__(self):
         return self.name
 
 class Services(models.Model):
     name = models.CharField(max_length=100)
+    price = models.CharField(max_length=100, default='SOME STRING')
 
     def __str__(self):
         return self.name
