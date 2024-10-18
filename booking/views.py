d6027a1bbc3d26f950b4d8f6e68ee763ea0db462
Jason Horgan <jasonahorgan@gmail.com>
2024-10-18 22:11:39 +0000

    Put views through linter, fixed errors and then pasted error free code back in so it will pass through linter without error

--- booking/views.py
+++ booking/views.py
@@ -6,16 +6,12 @@ from .forms import AppointmentForm
 from django.contrib.auth.decorators import login_required
 
 
-# Create your views here.
-
-#def my_barbershop(request):
- #   return HttpResponse("Hello, Barbershop!")
-
 def index(request):
     """
     Renders Home Page.
     """
     return render(request, "index.html",)
+
 
 def services(request):
     """
@@ -23,11 +19,13 @@ def services(request):
     """
     return render(request, "services.html",)
 
+
 def about(request):
     """
     Renders about page
     """
     return render(request, "about.html",)
+
 
 def appointments(request):
     """
@@ -35,31 +33,36 @@ def appointments(request):
     """
     return render(request, "appointments.html")
 
+
 def profile(request):
     """
     Renders appointments page
     """
     bookings = Appointment.objects.filter(author=request.user)
-    return render(request, "profile.html", 
-    {'bookings': bookings})
+    return render(request, "profile.html", {'bookings': bookings})
+
 
 @login_required
 def edit_appointment(request, appointment_id):
     """
     Allows users to edit an existing appointment.
     """
-    appointment = get_object_or_404(Appointment, id=appointment_id, author=request.user)
+    appointment = get_object_or_404(Appointment, id=appointment_id,
+                                    author=request.user)
 
     if request.method == 'POST':
         form = AppointmentForm(request.POST, instance=appointment)
         if form.is_valid():
             form.save()
-        messages.success(request, ('Your booking has been updated successfully'))
-        return redirect('profile') 
+        messages.success(request, ('Your booking has been' +
+                                   'updated successfully'))
+        return redirect('profile')
     else:
         form = AppointmentForm(instance=appointment)
 
-    return render(request, "edit_appointment.html", {"form": form, "appointment": appointment})
+    return render(request, "edit_appointment.html",
+                  {"form": form, "appointment": appointment})
+
 
 @login_required
 def book_appointment(request):
@@ -69,9 +72,9 @@ def book_appointment(request):
     if request.method == 'POST':
         form = AppointmentForm(request.POST)
         if form.is_valid():
-            appointment = form.save(commit=False)  # Create a new instance without saving it yet
-            appointment.author = request.user  # Set the author to the current user
-            appointment.save()  # Save the appointment to the database
+            appointment = form.save(commit=False)
+            appointment.author = request.user
+            appointment.save()
             return redirect('profile')
     else:
         form = AppointmentForm()
@@ -79,15 +82,16 @@ def book_appointment(request):
     return render(request, "book_appointment.html", {"form": form})
 
 
-        
 @login_required
 def delete_appointment(request, appointment_id):
-    appointment = get_object_or_404(Appointment, id=appointment_id, author=request.user)
+    appointment = get_object_or_404(Appointment, id=appointment_id,
+                                    author=request.user)
 
-    if request.method == 'POST':  # Handle the actual deletion only when the request is POST
+    if request.method == 'POST':
         appointment.delete()
         messages.success(request, 'Your booking has been cancelled')
         return redirect('profile')
-    
+
     # Render a confirmation page if the request is not a POST request
-    return render(request, "confirm_cancellation.html", {"appointment": appointment})
+    return render(request, "confirm_cancellation.html",
+                  {"appointment": appointment})
